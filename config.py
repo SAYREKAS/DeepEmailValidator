eadb0859606f6096d1559d9d9f4de50e1f1b0a59
Oleh Ohiichuk <chaotichoice@gmail.com>
2025-01-21 14:57:20 +0200

    Add logger configuration function, with a file and console output. Make proxy URL optional in ProxyParser. Add DNS server list configuration with default values. Update log level configuration with Literal type.

--- config.py
+++ config.py
@@ -1,15 +1,29 @@
 """Project configuration module."""
 
-import os.path
-from typing import Self
+import os
+import sys
+from typing import Self, Literal
 from urllib.parse import urlparse
 
-from pydantic import BaseModel, model_validator, Field
+from loguru import logger
+from pydantic import BaseModel, model_validator, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+def configure_logger(log_file: str) -> None:
+    """
+    Configures the logger to output logs to both the console and a file.
+    Logs will be displayed in the console at the configured log level and saved to a rotating log file with compression.
+    :param log_file: The name of the log file to store logs (e.g., "app.log").
+    """
+
+    logger.remove()
+    logger.add(sys.stdout, level=settings.logs_level, colorize=True)
+    logger.add(os.path.join(os.path.dirname(__file__), "logs", log_file), level=5, rotation="1 MB", compression="zip")
+
+
 class ProxyParser(BaseModel):  # type: ignore
-    url: str = Field(description="An expected format: 'protocol://username:password@proxy.example.com:8443'")
+    url: str | None = None
     protocol: str | None = None
     username: str | None = None
     password: str | None = None
@@ -19,6 +33,9 @@ class ProxyParser(BaseModel):  # type: ignore
     @model_validator(mode="after")  # type: ignore
     def validate_url(self) -> Self:
         """Validate URL and parse its components"""
+
+        if self.url is None:
+            return self
 
         parsed_url = urlparse(self.url)
 
@@ -58,7 +75,16 @@ class Settings(BaseSettings):  # type: ignore
         env_prefix="APP__",
         env_nested_delimiter="__",
     )
-    proxy: ProxyParser = Field(description="Proxy settings")
+    proxy: ProxyParser | None = None
+    dns_server_list: list[str] | None = None
+    logs_level: Literal["TRACE", "DEBUG", "INFO", "SUCCESS", "WARNING", "ERROR", "CRITICAL"] = "INFO"
+
+    @field_validator("dns_server_list", mode="before")  # type: ignore
+    def parse_dns_servers_from_env(cls, value: str) -> list[str]:
+        """Parse DNS server list from environment variable"""
+        if value:
+            return value.split(",")
+        return ["8.8.8.8", "8.8.4.4"]  # default google dns
 
 
 settings = Settings()
